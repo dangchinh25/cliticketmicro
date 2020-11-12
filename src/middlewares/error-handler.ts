32d45418d561583fda216706e18fcc564df1165e
DangChinh <dangchinh2501@gmail.com>
2020-11-12 15:22:42 -0600

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,12 +1,21 @@
-import { Request, Response, NextFunction } from "express"
-import { CustomError } from "../errors/custom-error"
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
 
-export const errorHandle = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof CustomError) {
-        return res.status(err.statusCode).send({ errors: err.serializeError() })
-    }
+export const errorHandle = (
+	err: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	if (err instanceof CustomError) {
+		return res
+			.status(err.statusCode)
+			.send({ errors: err.serializeError() });
+	}
 
-    res.status(400).send({
-        errors: [{ message: "Something went wrong" }]
-    })
-}
+	console.error(err);
+
+	res.status(400).send({
+		errors: [{ message: 'Something went wrong' }],
+	});
+};
